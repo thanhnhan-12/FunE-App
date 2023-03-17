264f33a3be7487fe1ffd6a2bff13b99ac3a5e2d3
huykt1111 <huylmht10@gmail.com>
2023-03-17 16:15:58 +0700

    login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,22 @@
-import React, { useState, useContext } from "react";
-import { Button, Text, TextInput, TouchableOpacity, View, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import LoginSVG from '../assets/images/misc/login.svg';
+import GoogleSVG from '../assets/images/misc/google.svg';
+import FacebookSVG from '../assets/images/misc/facebook.svg';
+import TwitterSVG from '../assets/images/misc/twitter.svg';
+
+// import { CustomButton } from '../components/CustomButton';
+import { InputField } from '../components/InputField';
+
 import { AuthContext } from "../context/AuthContext";
 import Spinner from "react-native-loading-spinner-overlay";
 
@@ -9,61 +26,192 @@ const LoginScreen = ({ navigation }) => {
     const { login, isLoading } = useContext(AuthContext);
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
             <Spinner visible={isLoading} />
-            <View style={styles.wrapper}>
-                <TextInput
-                    style={styles.input}
-                    value={email}
-                    placeholder="Enter email"
-                    onChangeText={text => setEmail(text)}
-                />
+            <View style={{ paddingHorizontal: 25 }}>
+                <View style={{ alignItems: 'center' }}>
+                    <LoginSVG
+                        height={300}
+                        width={300}
+                        style={{ transform: [{ rotate: '-5deg' }] }}
+                    />
+                </View>
 
-                <TextInput
-                    style={styles.input}
-                    value={password}
-                    placeholder="Enter password"
-                    secureTextEntry
-                    onChangeText={text => setPassword(text)}
-                />
+                <Text
+                    style={{
+                        fontFamily: 'Roboto-Medium',
+                        fontSize: 28,
+                        fontWeight: '500',
+                        color: '#333',
+                        marginBottom: 30,
+                    }}>
+                    Login
+                </Text>
 
-                <Button
-                    title="Login"
-                    onPress={() => { login(email, password) }}
-                />
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        borderBottomColor: '#ccc',
+                        borderBottomWidth: 1,
+                        paddingBottom: 8,
+                        marginBottom: 25,
+                    }}>
+                    <MaterialIcons
+                        name="person"
+                        size={20}
+                        color="#666"
+                        style={{ marginRight: 5 }}
+                    />
+                    <TextInput
+                        placeholder={'Email ID'}
+                        keyboardType={"email-address"}
+                        value={email}
+                        style={{ flex: 1, paddingVertical: 0 }}
+                        onChangeText={text => setEmail(text)}
+                        secureTextEntry={true}
+                    />
+                    <TouchableOpacity>
+                        <Text style={{ color: '#AD40AF', fontWeight: '700' }}></Text>
+                    </TouchableOpacity>
+                </View>
 
-                <View style={{ flexDirection: 'row', marginTop: 20 }}>
-                    <Text>Don't have an account? </Text>
+                {/* <InputField
+                    label={'Email ID'}
+                    icon={
+                        <MaterialIcons
+                            name="alternate-email"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5 }}
+                        />
+                    }
+                    keyboardType="email-address"
+                /> */}
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        borderBottomColor: '#ccc',
+                        borderBottomWidth: 1,
+                        paddingBottom: 8,
+                        marginBottom: 25,
+                    }}>
+                    <Ionicons
+                        name="ios-lock-closed-outline"
+                        size={20}
+                        color="#666"
+                        style={{ marginRight: 5 }}
+                    />
+                    <TextInput
+                        placeholder={'Password'}
+                        secureTextEntry
+                        value={password}
+                        style={{ flex: 1, paddingVertical: 0 }}
+                        onChangeText={text => setPassword(text)}
+                    />
+                    <TouchableOpacity>
+                        <Text style={{ color: '#AD40AF', fontWeight: '700' }}>Forgot?</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* <InputField
+                    label={'Password'}
+                    icon={
+                        <Ionicons
+                            name="ios-lock-closed-outline"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5 }}
+                        />
+                    }
+                    inputType="password"
+                    fieldButtonLabel={"Forgot?"}
+                    fieldButtonFunction={() => { }}
+                /> */}
+
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#AD40AF',
+                        padding: 20,
+                        borderRadius: 10,
+                        marginBottom: 30,
+                    }}>
+                    <Text
+                        onPress={() => { login(email, password) }}
+                        style={{
+                            textAlign: 'center',
+                            fontWeight: '700',
+                            fontSize: 16,
+                            color: '#fff',
+                        }}>
+                        Login
+                    </Text>
+                </TouchableOpacity>
+                {/* <CustomButton label={"Login"} onPress={() => { }} /> */}
+
+                <Text style={{ textAlign: 'center', color: '#666', marginBottom: 30 }}>
+                    Or, login with ...
+                </Text>
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        marginBottom: 30,
+                    }}>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{
+                            borderColor: '#ddd',
+                            borderWidth: 2,
+                            borderRadius: 10,
+                            paddingHorizontal: 30,
+                            paddingVertical: 10,
+                        }}>
+                        <GoogleSVG height={24} width={24} />
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{
+                            borderColor: '#ddd',
+                            borderWidth: 2,
+                            borderRadius: 10,
+                            paddingHorizontal: 30,
+                            paddingVertical: 10,
+                        }}>
+                        <FacebookSVG height={24} width={24} />
+
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{
+                            borderColor: '#ddd',
+                            borderWidth: 2,
+                            borderRadius: 10,
+                            paddingHorizontal: 30,
+                            paddingVertical: 10,
+                        }}>
+
+                        <TwitterSVG height={24} width={24} />
+
+                    </TouchableOpacity>
+                </View>
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        marginBottom: 30,
+                    }}>
+                    <Text>New to the app?</Text>
                     <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-                        <Text style={styles.link}>Register</Text>
+                        <Text style={{ color: '#AD40AF', fontWeight: '700' }}> Register</Text>
                     </TouchableOpacity>
                 </View>
             </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create(
-    {
-        container: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        wrapper: {
-            width: '80%'
-        },
-        input: {
-            marginBottom: 12,
-            borderWidth: 1,
-            borderColor: '#bbb',
-            borderRadius: 5,
-            paddingHorizontal: 14,
-        },
-        link: {
-            color: 'blue',
-        }
-    }
-)
+        </SafeAreaView>
+    );
+};
 
 export default LoginScreen;
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,89 +1,313 @@
-import React, { useState, useContext } from "react";
-import { Button, Text, TextInput, TouchableOpacity, View, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import Toast from 'react-native-toast-message';
+import {
+    SafeAreaView,
+    ScrollView,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+import InputField from '../components/InputField';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import RegistrationSVG from '../assets/images/misc/registration.svg';
+import GoogleSVG from '../assets/images/misc/google.svg';
+import FacebookSVG from '../assets/images/misc/facebook.svg';
+import TwitterSVG from '../assets/images/misc/twitter.svg';
+import CustomButton from '../components/CustomButton';
 import { AuthContext } from "../context/AuthContext";
 import Spinner from "react-native-loading-spinner-overlay";
 
 const RegisterScreen = ({ navigation }) => {
+    const [date, setDate] = useState(new Date());
+    const [open, setOpen] = useState(false);
+    const [dobLabel, setDobLabel] = useState('Date of Birth');
     const [firstName, setFirstName] = useState(null);
     const [lastName, setLastName] = useState(null);
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
 
     const { register, isLoading } = useContext(AuthContext);
 
+    const formValidation = () => {
+        setSuccess("")
+        // input validation
+        if (password == null) {
+            setError("Password is required feild");
+        } else if (password !== confirmPassword) {
+            setError("Passwoad and confirm password should be same.");
+        }
+        else {
+            setError("");
+            let birthday = dobLabel;
+            try {
+                register(firstName, lastName, email, password, birthday);
+                setSuccess("Register Succeed!");
+                setFirstName("");
+                setLastName("");
+                setEmail("");
+                setPassword("");
+                setConfirmPassword("");
+            }
+            catch (e) {
+                setError("Register Error!");
+            }
+
+        }
+    }
+
     return (
+        <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={{ paddingHorizontal: 25 }}>
+                <View style={{ alignItems: 'center' }}>
+                    <RegistrationSVG
+                        height={300}
+                        width={300}
+                        style={{ transform: [{ rotate: '-5deg' }] }}
+                    />
+                </View>
 
-        <View style={styles.container}>
-            <Spinner visible={isLoading} />
-            <View style={styles.wrapper}>
-                <TextInput
-                    style={styles.input}
-                    value={firstName}
-                    placeholder="Enter first name"
-                    onChangeText={text => setFirstName(text)}
-                />
+                <Text
+                    style={{
+                        fontFamily: 'Roboto-Medium',
+                        fontSize: 28,
+                        fontWeight: '500',
+                        color: '#333',
+                        marginBottom: 30,
+                    }}>
+                    Register
+                </Text>
 
-                <TextInput
-                    style={styles.input}
-                    value={lastName}
-                    placeholder="Enter last name"
-                    onChangeText={text => setLastName(text)}
-                />
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        marginBottom: 30,
+                    }}>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{
+                            borderColor: '#ddd',
+                            borderWidth: 2,
+                            borderRadius: 10,
+                            paddingHorizontal: 30,
+                            paddingVertical: 10,
+                        }}>
+                        <GoogleSVG height={24} width={24} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{
+                            borderColor: '#ddd',
+                            borderWidth: 2,
+                            borderRadius: 10,
+                            paddingHorizontal: 30,
+                            paddingVertical: 10,
+                        }}>
+                        <FacebookSVG height={24} width={24} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{
+                            borderColor: '#ddd',
+                            borderWidth: 2,
+                            borderRadius: 10,
+                            paddingHorizontal: 30,
+                            paddingVertical: 10,
+                        }}>
+                        <TwitterSVG height={24} width={24} />
+                    </TouchableOpacity>
+                </View>
 
-                <TextInput
-                    style={styles.input}
+                <Text style={{ textAlign: 'center', color: '#666', marginBottom: 30 }}>
+                    Or, register with email ...
+                </Text>
+                {
+                    error != null ?
+                        (
+                            <Text style={{ textAlign: 'center', color: 'red' }}>
+                                {error}
+                            </Text>
+                        )
+                        :
+                        ''
+                }
+
+                {
+                    success != null ?
+                        (
+                            <Text style={{ textAlign: 'center', color: '#AD40AF' }}>
+                                {success}
+                            </Text>
+                        )
+                        :
+                        ''
+                }
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        marginBottom: 30,
+                        gap: 10
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            borderBottomColor: '#ccc',
+                            borderBottomWidth: 1,
+                            paddingBottom: 8,
+
+                        }}
+                    >
+                        <Ionicons
+                            name="person-outline"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5, marginTop: 10 }}
+                        />
+                        <TextInput
+                            placeholder="Last Name"
+                            width="55%"
+                            value={lastName}
+                            onChangeText={text => setLastName(text)}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            borderBottomColor: '#ccc',
+                            borderBottomWidth: 1,
+                            paddingBottom: 8,
+                        }}
+                    >
+                        <Ionicons
+                            name="person-outline"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5, marginTop: 10 }}
+                        />
+                        <TextInput
+                            placeholder="First Name"
+                            width="45%"
+                            value={firstName}
+                            onChangeText={text => setFirstName(text)}
+                        />
+                    </View>
+
+                </View>
+
+                <InputField
                     value={email}
-                    placeholder="Enter email"
                     onChangeText={text => setEmail(text)}
+                    label={'Email ID'}
+                    icon={
+                        <MaterialIcons
+                            name="alternate-email"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5 }}
+                        />
+                    }
+                    keyboardType="email-address"
                 />
 
-                <TextInput
-                    style={styles.input}
-                    value={password}
-                    placeholder="Enter password"
-                    secureTextEntry
+                <InputField
                     onChangeText={text => setPassword(text)}
+                    value={password}
+                    label={'Password'}
+                    icon={
+                        <Ionicons
+                            name="ios-lock-closed-outline"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5 }}
+                        />
+                    }
+                    inputType="password"
                 />
 
-                <Button
-                    title="Register"
+                <InputField
+                    onChangeText={text => setConfirmPassword(text)}
+                    value={confirmPassword}
+                    label={'Confirm Password'}
+                    icon={
+                        <Ionicons
+                            name="ios-lock-closed-outline"
+                            size={20}
+                            color="#666"
+                            style={{ marginRight: 5 }}
+                        />
+                    }
+                    inputType="password"
+                />
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        borderBottomColor: '#ccc',
+                        borderBottomWidth: 1,
+                        paddingBottom: 8,
+                        marginBottom: 30,
+                    }}>
+                    <Ionicons
+                        name="calendar-outline"
+                        size={20}
+                        color="#666"
+                        style={{ marginRight: 5 }}
+                    />
+                    <TouchableOpacity onPress={() => setOpen(true)}>
+                        <Text style={{ color: '#666', marginLeft: 5, marginTop: 5 }}>
+                            {dobLabel}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <DatePicker
+                    modal
+                    open={open}
+                    date={date}
+                    mode={'date'}
+                    maximumDate={new Date('2005-01-01')}
+                    minimumDate={new Date('1980-01-01')}
+                    onConfirm={date => {
+                        setOpen(false);
+                        setDate(date);
+                        setDobLabel(date.toDateString());
+                    }}
+                    onCancel={() => {
+                        setOpen(false);
+                    }}
+                />
+                <CustomButton
+                    label={'Register'}
                     onPress={() => {
-                        register(firstName, lastName, email, password)
+                        formValidation()
                     }}
                 />
 
-                <View style={{ flexDirection: 'row', marginTop: 20 }}>
-                    <Text>Already have an account? </Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                        <Text style={styles.link}>Login</Text>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        marginBottom: 30,
+                    }}>
+                    <Text>Already registered?</Text>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Text style={{ color: '#AD40AF', fontWeight: '700' }}> Login</Text>
                     </TouchableOpacity>
                 </View>
-            </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create(
-    {
-        container: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        wrapper: {
-            width: '80%'
-        },
-        input: {
-            marginBottom: 12,
-            borderWidth: 1,
-            borderColor: '#bbb',
-            borderRadius: 5,
-            paddingHorizontal: 14,
-        },
-        link: {
-            color: 'blue',
-        }
-    }
-)
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
 
 export default RegisterScreen;
