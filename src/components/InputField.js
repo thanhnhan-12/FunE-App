a006ec7dbc7b59359e674415e1031baa6654b12d
vanthienpham <vanthien.dev@gmail.com>
2023-03-18 17:49:41 +0700

    fix change text register

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 
 export default function InputField({
   label,
@@ -8,6 +8,7 @@ export default function InputField({
   keyboardType,
   fieldButtonLabel,
   fieldButtonFunction,
+  onChangeText
 }) {
   return (
     <View
@@ -23,18 +24,20 @@ export default function InputField({
         <TextInput
           placeholder={label}
           keyboardType={keyboardType}
-          style={{flex: 1, paddingVertical: 0}}
+          style={{ flex: 1, paddingVertical: 0 }}
           secureTextEntry={true}
+          onChangeText={onChangeText}
         />
       ) : (
         <TextInput
           placeholder={label}
           keyboardType={keyboardType}
-          style={{flex: 1, paddingVertical: 0}}
+          style={{ flex: 1, paddingVertical: 0 }}
+          onChangeText={onChangeText}
         />
       )}
       <TouchableOpacity onPress={fieldButtonFunction}>
-        <Text style={{color: '#AD40AF', fontWeight: '700'}}>{fieldButtonLabel}</Text>
+        <Text style={{ color: '#AD40AF', fontWeight: '700' }}>{fieldButtonLabel}</Text>
       </TouchableOpacity>
     </View>
   );
