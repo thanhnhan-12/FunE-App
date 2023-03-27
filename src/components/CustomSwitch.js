35b2d8fde42166bbd6fb2257016906220fc6085e
huykt1111 <huylmht10@gmail.com>
2023-03-27 20:50:47 +0700

    Home - Search Frontend

--- src/components/CustomSwitch.js
+++ src/components/CustomSwitch.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function CustomSwitch({
   selectionMode,
   option1,
   option2,
+  icon1,
+  icon2,
   onSelectSwitch,
 }) {
   const [getSelectionMode, setSelectionMode] = useState(selectionMode);
@@ -19,7 +21,7 @@ export default function CustomSwitch({
       style={{
         height: 44,
         width: '100%',
-        backgroundColor: '#e4e4e4',
+        backgroundColor: 'white',
         borderRadius: 10,
         borderColor: '#AD40AF',
         flexDirection: 'row',
@@ -30,18 +32,21 @@ export default function CustomSwitch({
         onPress={() => updateSwitchData(1)}
         style={{
           flex: 1,
-          backgroundColor: getSelectionMode == 1 ? '#AD40AF' : '#e4e4e4',
-          borderRadius: 10,
+          // backgroundColor: getSelectionMode == 1 ? '#AD40AF' : 'white',
+          // borderRadius: 10,
           justifyContent: 'center',
           alignItems: 'center',
-        }}>
+        }}
+      >
         <Text
           style={{
-            color: getSelectionMode == 1 ? 'white' : '#AD40AF',
-            fontSize: 14,
-            fontFamily: 'Roboto-Medium',
+            // color: getSelectionMode == 1 ? 'white' : '#AD40AF',
+            // fontSize: 14,
+            // fontFamily: 'Roboto-Medium',
+            borderBottomWidth: getSelectionMode == 1 ? 3 : 0,
           }}>
           {option1}
+          {icon1}
         </Text>
       </TouchableOpacity>
       <TouchableOpacity
@@ -49,18 +54,20 @@ export default function CustomSwitch({
         onPress={() => updateSwitchData(2)}
         style={{
           flex: 1,
-          backgroundColor: getSelectionMode == 2 ? '#AD40AF' : '#e4e4e4',
-          borderRadius: 10,
+          // backgroundColor: getSelectionMode == 2 ? '#AD40AF' : 'white',
+          // borderRadius: 10,
           justifyContent: 'center',
           alignItems: 'center',
         }}>
         <Text
           style={{
-            color: getSelectionMode == 2 ? 'white' : '#AD40AF',
-            fontSize: 14,
-            fontFamily: 'Roboto-Medium',
+            // color: getSelectionMode == 2 ? 'white' : '#AD40AF',
+            // fontSize: 14,
+            // fontFamily: 'Roboto-Medium',
+            borderBottomWidth: getSelectionMode == 2 ? 3 : 0,
           }}>
           {option2}
+          {icon2}
         </Text>
       </TouchableOpacity>
     </View>
