6a70e7f2be00d2ae7e8883b30e597f1fea49eda2
huykt1111 <huylmht10@gmail.com>
2023-04-03 16:17:24 +0700

    Room

--- src/clients/room_api.js
+++ src/clients/room_api.js
@@ -3,10 +3,19 @@ export const roomApi = {
     createRoom: async (data) => {
         return await instance.post('/room/create', data);
     },
+    joinRoom: async (data) => {
+        return await instance.post('/room/join', data);
+    },
     getRoomCreate: async (data) => {
         return await instance.get(`/room/get/create?id=${data.id_user}`);
     },
+    getRoomJoined: async (data) => {
+        return await instance.get(`/room/get/joined?id=${data.id_user}`);
+    },
     getRoomNotJoin: async (data) => {
         return await instance.get(`/room/get/not-joined?id=${data.id_user}`);
-    }
+    },
+    getMembers: async (data) => {
+        return await instance.get(`/room/get/members?id=${data}`);
+    },
 }
---
+++ src/screens/modal/ModalPostRoom.js
@@ -0,0 +1,201 @@
+import React, { useState } from 'react';
+import { Alert, Modal, StyleSheet, Text, Pressable, View, TouchableOpacity } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CreatePostModalRoom from '../CreatePost/CreatePostModalRoom';
+
+
+const handleOnClick = (modalVisible, setModalVisible, opacityModal, setOpacityModal) => {
+    setModalVisible(!modalVisible);
+    setOpacityModal(!opacityModal);
+}
+
+const ModalPostRoom = ({ modalVisible, setModalVisible, opacityModal, setOpacityModal, setRoom, navigation }) => {
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setModalVisible(!modalVisible);
+                setOpacityModal(!opacityModal);
+            }}>
+            <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+
+                    <TouchableOpacity style={{
+                        marginLeft: 120
+                    }}>
+                        <Ionicons
+                            onPress={() => handleOnClick(modalVisible, setModalVisible, opacityModal, setOpacityModal)}
+                            name="close"
+                            size={40}
+                            color="red"
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                            setOpacityModal(!opacityModal);
+                        }}
+                        style={{
+                            alignItems: 'center',
+                            textAlign: 'center',
+                            padding: 10,
+                            width: '60%',
+                            flexDirection: 'row',
+                            justifyContent: 'space-evenly',
+
+                        }}
+                    >
+                        <CreatePostModalRoom>
+                            <>
+                                <Text
+                                    style={{
+                                        color: 'black',
+                                        fontSize: 16,
+                                        textAlign: 'center',
+                                        fontFamily: 'Roboto-MediumItalic',
+                                        width: "100%",
+                                    }}>
+                                    Add Pic/Video
+                                </Text>
+                                <MaterialIcons name="arrow-forward-ios" size={22} color="red" />
+                            </>
+                        </CreatePostModalRoom>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            alignItems: 'center',
+                            textAlign: 'center',
+                            padding: 10,
+                            width: '60%',
+                            flexDirection: 'row',
+                            justifyContent: 'space-evenly',
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: 'black',
+                                fontSize: 16,
+                                textAlign: 'center',
+                                fontFamily: 'Roboto-MediumItalic',
+                                width: "80%"
+
+                            }}>
+                            Add Livestream
+                        </Text>
+
+                        <MaterialIcons name="arrow-forward-ios" size={22} color="red" />
+                    </TouchableOpacity>
+                    {setRoom === false &&
+                        <View>
+                            <TouchableOpacity
+                                style={{
+                                    alignItems: 'center',
+                                    textAlign: 'center',
+                                    padding: 10,
+                                    width: '60%',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-evenly',
+                                }}
+                            >
+                                <Text
+                                    style={{
+                                        color: 'black',
+                                        fontSize: 16,
+                                        textAlign: 'center',
+                                        fontFamily: 'Roboto-MediumItalic',
+                                        width: "80%"
+
+                                    }}>
+                                    Choose Products
+                                </Text>
+
+                                <MaterialIcons name="arrow-forward-ios" size={22} color="red" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{
+                                    alignItems: 'center',
+                                    textAlign: 'center',
+                                    padding: 10,
+                                    width: '60%',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-evenly',
+                                }}
+                            >
+                                <Text
+                                    style={{
+                                        color: 'black',
+                                        fontSize: 16,
+                                        textAlign: 'center',
+                                        fontFamily: 'Roboto-MediumItalic',
+                                        width: "80%"
+
+                                    }}
+                                    onPress={() => {
+                                        setOpacityModal(!opacityModal);
+                                        setModalVisible(!modalVisible);
+                                        navigation.navigate('CreateProduct');
+                                    }}
+                                >
+                                    Add Product
+                                </Text>
+
+                                <MaterialIcons name="arrow-forward-ios" size={22} color="red" />
+                            </TouchableOpacity>
+                        </View>
+                    }
+                </View>
+            </View>
+        </Modal >
+
+    );
+};
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+});
+
+export default ModalPostRoom;
